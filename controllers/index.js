6d838f448b90454e64d00157c520ded29adca647
Anillag <101154249+anillag@users.noreply.github.com>
2022-07-20 20:58:29 -0400

    Basic layout

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,13 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const apiRoutes = require('./api');
-//const homeRoutes = require('./home-routes.js');
+const apiRoutes = require("./api");
+const homeRoutes = require("./home-routes.js");
 
-//router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
+router.use("/", homeRoutes);
+router.use("/api", apiRoutes);
 
 router.use((req, res) => {
-    res.send("<h1>Wrong Route!</h1>")
+  res.send("<h1>Wrong Route!</h1>");
 });
 
 module.exports = router;
