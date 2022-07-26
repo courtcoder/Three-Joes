127ae9fbdb5fbeec155556daa8a182619823da79
courtcoder <courtney1187@icloud.com>
2022-07-26 16:30:38 -0400

    added styling to main page

--- config/connection.js
+++ config/connection.js
@@ -5,22 +5,27 @@ require("dotenv").config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PWD, {
-        host: "localhost",
-        dialect: "mysql",
-        port: 3306
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PWD,
+    {
+      host: "127.0.0.1",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+
+  const connDB = sequelize
+    .authenticate()
+    .then(function () {
+      console.log("Connected Successfully!");
+    })
+    .catch(function (err) {
+      console.log("Connection Failed!");
     });
-
-    const connDB = sequelize.authenticate()
-        .then(function() {
-            console.log("Connected Successfully!");
-        })
-        .catch(function(err) {
-            console.log("Connection Failed!");
-        })
-
 }
 
 module.exports = sequelize;
