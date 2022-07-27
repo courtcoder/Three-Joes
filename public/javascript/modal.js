b784892f68c3e8fb08ac26202e05eeee4b7d715a
Joseph Darville <joseph.darville9@gmail.com>
2022-07-26 23:53:07 -0400

    added modal to main page

---
+++ public/javascript/modal.js
@@ -0,0 +1,27 @@
+const cartButton = document.querySelector("#cart-button");
+const submitOrder = document.querySelector("#submit-order");
+const exitModal = document.querySelectorAll("#exit-modal");
+
+for (i of exitModal) {
+  i.addEventListener("click", function () {
+    closeModal();
+  });
+}
+
+function activateModal() {
+  console.log("button clicked");
+  const modal = document.querySelector(".modal");
+  modal.classList.add("is-active");
+}
+
+function proceedToPayment() {
+  console.log("this works");
+}
+
+function closeModal() {
+  const modal = document.querySelector(".modal");
+  modal.classList.remove("is-active");
+}
+
+cartButton.addEventListener("click", activateModal);
+submitOrder.addEventListener("click", proceedToPayment);
