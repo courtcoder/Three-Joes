f1697ef6ba5b8f0f69c36ad230daa251f2bd5435
Joseph Darville <joseph.darville9@gmail.com>
2022-07-30 01:19:08 -0400

    fixed modal item population

--- public/javascript/create-order.js
+++ public/javascript/create-order.js
@@ -1,70 +1,37 @@
 var buttonClicked = document.querySelector("#menu-button");
-var orderArr = [ ];    
-var buttonHandler = function(event) {
-    event.preventDefault();
-    
-    var wtf = event.target.id;
-    //alert(wtf);
-    console.log(wtf);
-    getProductData(wtf);
+var orderArr = [];
+var buttonHandler = function (event) {
+  event.preventDefault();
+
+  var wtf = event.target.id;
+  //alert(wtf);
+  getProductData(wtf);
 };
 
-
 function getProductData(pid) {
-    
-    fetch(`/api/products/${(pid)}`)
-    .then(function(response) {
-        return response.json();
+  fetch(`/api/products/${pid}`)
+    .then(function (response) {
+      return response.json();
     })
-    .then(function(data) {
-        
-        var id = data.id;
-        var prod_name = data.prod_name;
-        var prod_desc = data.prod_desc;
-        var img_id = data.img_id;
-        var price = data.price;
+    .then(function (data) {
+      var id = data.id;
+      var prod_name = data.prod_name;
+      var prod_desc = data.prod_desc;
+      var img_id = data.img_id;
+      var price = data.price;
 
-        var orderLine = {
-            id: id,
-            prod_name: prod_name,
-            prod_desc: prod_desc,
-            img_id: img_id,
-            price: price
-        };
+      var orderLine = {
+        id: id,
+        prod_name: prod_name,
+        prod_desc: prod_desc,
+        img_id: img_id,
+        price: price,
+      };
 
+      orderArr.push(orderLine);
 
-        // just tests to confirm proper functionality
-        // console.log(`orderLine: ${orderLine}`);
-
-        orderArr.push(orderLine);
-
-        console.log(`orderArr: ${JSON.stringify(orderArr)}`);
-
-        // pushing data to the modal, but this doesn't display in the modal as the modal is part of the main.handlebars page. Was just a test to see if
-        // I could call to another page.
-        // addCartItem(orderArr);
-
+      addCartItem(orderArr);
     });
 }
 
-
-
-
-
-
-
-
-
 buttonClicked.addEventListener("click", buttonHandler);
-
-
-
-
-
-
-
-
-
-
-
-
--- public/javascript/modal.js
+++ public/javascript/modal.js
@@ -2,6 +2,9 @@ const cartButton = document.querySelector("#cart-button");
 const submitOrder = document.querySelector("#submit-order");
 const exitModal = document.querySelectorAll("#exit-modal");
 
+let prodArray = [];
+let htmlArr = [];
+
 for (i of exitModal) {
   i.addEventListener("click", function () {
     closeModal();
@@ -9,17 +12,37 @@ for (i of exitModal) {
 }
 
 function activateModal() {
+  htmlArr = [];
+  document.getElementById("product-display").innerHTML = "";
+
   console.log("button clicked");
   const modal = document.querySelector(".modal");
   modal.classList.add("is-active");
+
+  prodArray.forEach((item) => {
+    console.log(item[1]);
+    let htmltemplate = `<div class="column is-one-quarter ">
+        <div class="box has-text-centered">
+          <img src="../images/${item[1]}.jpg" alt="Def not a kitty" />
+          <div class="mt-3">${item[0]}</div>
+        </div>
+      </div>`;
+
+    htmlArr.push(htmltemplate);
+  });
+
+  console.log(htmlArr);
+  document.getElementById("product-display").innerHTML = htmlArr.join("");
 }
 
 function addCartItem(item) {
-  // test to make sure it reaches the modal script
-  console.log(`item on modal: ${JSON.stringify(item)}`);
-  
-
-} 
+  console.log(item);
+  prodArray = [];
+  for (let i = 0; i < item.length; i++) {
+    const newItem = Object.values(item[i]);
+    prodArray.push([newItem[1], newItem[3]]);
+  }
+}
 
 function proceedToPayment() {
   console.log("this works");
